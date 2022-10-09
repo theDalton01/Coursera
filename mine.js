316f4d442ab35dd2c44967e17223c8e24c50d5c0
theDalton01 <dabeejay691@gmail.com>
2022-10-09 13:15:41 +0100

    assignment

---
+++ mine.js
@@ -0,0 +1,13 @@
+
+console.log(document.getElementById("WTF"));
+
+// var a = "babe"
+// b = a
+
+//  console.log(b);
+// console.log(document.getElementById("charm"));
+
+function Bodycount () {
+	console.log(document.getElementById("male").value
+		);
+}
